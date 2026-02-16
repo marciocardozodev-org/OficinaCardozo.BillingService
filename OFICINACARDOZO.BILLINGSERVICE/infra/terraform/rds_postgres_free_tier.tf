0fb5394b1dd5e912f144d6d87c00ba0dd0f285ce
marciocardozodev <60279810+marciocardozodev@users.noreply.github.com>
2026-02-16 16:54:06 +0000

    feat: atualiza configurações do RDS para o BillingService e OSService, renomeando variáveis e outputs

--- OFICINACARDOZO.BILLINGSERVICE/infra/terraform/rds_postgres_free_tier.tf
+++ OFICINACARDOZO.BILLINGSERVICE/infra/terraform/rds_postgres_free_tier.tf
@@ -1,26 +1,26 @@
 # RDS PostgreSQL Free Tier
-resource "aws_db_instance" "main" {
+resource "aws_db_instance" "billingservice" {
   allocated_storage    = 20
   engine               = "postgres"
   instance_class       = "db.t3.micro"
-  db_name              = var.db_name
-  username             = var.db_username
-  password             = var.db_password
+  db_name              = var.billingservice_db_name
+  username             = var.billingservice_db_username
+  password             = var.billingservice_db_password
   # parameter_group_name removido para usar o padrão da AWS
-  db_subnet_group_name = aws_db_subnet_group.main.name
-  vpc_security_group_ids = var.db_security_group_ids
+  db_subnet_group_name = aws_db_subnet_group.billingservice.name
+  vpc_security_group_ids = var.billingservice_db_security_group_ids
   skip_final_snapshot  = true
   publicly_accessible  = false
   storage_encrypted    = true
   tags = {
-    Name = "${var.app_name}-rds-postgres"
+    Name = "${var.billingservice_app_name}-rds-postgres"
   }
 }
 
-resource "aws_db_subnet_group" "main" {
-  name       = "${var.app_name}-rds-subnet-group"
-  subnet_ids = var.db_subnet_ids
+resource "aws_db_subnet_group" "billingservice" {
+  name       = "${var.billingservice_app_name}-rds-subnet-group"
+  subnet_ids = var.billingservice_db_subnet_ids
   tags = {
-    Name = "${var.app_name}-rds-subnet-group"
+    Name = "${var.billingservice_app_name}-rds-subnet-group"
   }
 }

aa9edf7fe3894872d2a6dd232e0a180d55a99dc9
marciocardozodev <60279810+marciocardozodev@users.noreply.github.com>
2026-02-16 23:15:29 +0000

    feat: adiciona variável billingservice_rds_instance_id e ajusta nomes de variáveis no workflow CI/CD

--- infra/terraform/rds_postgres_free_tier.tf
+++ infra/terraform/rds_postgres_free_tier.tf
@@ -1,17 +1,18 @@
 # RDS PostgreSQL Free Tier
 resource "aws_db_instance" "billingservice" {
-  allocated_storage    = 20
-  engine               = "postgres"
-  instance_class       = "db.t3.micro"
-  db_name              = var.billingservice_db_name
-  username             = var.billingservice_db_username
-  password             = var.billingservice_db_password
+  identifier              = var.billingservice_rds_instance_id
+  allocated_storage       = 20
+  engine                  = "postgres"
+  instance_class          = "db.t3.micro"
+  db_name                 = var.billingservice_db_name
+  username                = var.billingservice_db_username
+  password                = var.billingservice_db_password
   # parameter_group_name removido para usar o padrão da AWS
-  db_subnet_group_name = aws_db_subnet_group.billingservice.name
-  vpc_security_group_ids = var.billingservice_db_security_group_ids
-  skip_final_snapshot  = true
-  publicly_accessible  = false
-  storage_encrypted    = true
+  db_subnet_group_name    = aws_db_subnet_group.billingservice.name
+  vpc_security_group_ids  = var.billingservice_db_security_group_ids
+  skip_final_snapshot     = true
+  publicly_accessible     = false
+  storage_encrypted       = true
   tags = {
     Name = "${var.billingservice_app_name}-rds-postgres"
   }

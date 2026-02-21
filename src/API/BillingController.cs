1048eb3232256ab164e809935834c80f103c52a3
marciocardozodev <60279810+marciocardozodev@users.noreply.github.com>
2026-02-21 21:49:31 +0000

    feat: adicionar using para Entity Framework no BillingController

--- src/API/BillingController.cs
+++ src/API/BillingController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 using OFICINACARDOZO.BILLINGSERVICE.Application;
 using OFICINACARDOZO.BILLINGSERVICE.Domain;
 
